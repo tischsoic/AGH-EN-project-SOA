857b8d7c89fb677653b20e359d64dc5d7d1b39ee
Jakub Brzegowski <symfiz@gmail.com>
2017-06-09 19:20:03 +0200

    Deleted files from target directory in module mHib.

--- mHib/target/classes/database_setup.sql
+++
@@ -1,9 +0,0 @@
-create table book (
-  id integer primary key,
-  title char(32),
-  authorFirstName char(32),
-  authorSecondName char(32),
-  isbn char(32),
-  publicationDate integer,
-  price numeric(7,2)
-);
